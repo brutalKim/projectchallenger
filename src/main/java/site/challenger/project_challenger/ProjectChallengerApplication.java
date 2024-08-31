7a42563dccc8383125612c67e6524025400f38c4
brutalKim <132333588+brutalKim@users.noreply.github.com>
2024-08-31 21:33:25 +0900

    basic main branch

---
+++ src/main/java/site/challenger/project_challenger/ProjectChallengerApplication.java
@@ -0,0 +1,13 @@
+package site.challenger.project_challenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectChallengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/challenger/project_challenger/ProjectChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.challenger.project_challenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
