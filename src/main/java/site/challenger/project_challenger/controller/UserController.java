0c3658fc6f8394ae7caf99f8b6c7658f4ccc3405
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2024-10-10 14:07:13 +0900

    insu1010-7840-1

--- src/main/java/site/challenger/project_challenger/controller/UserController.java
+++ src/main/java/site/challenger/project_challenger/controller/UserController.java
@@ -1,5 +1,8 @@
 package site.challenger.project_challenger.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +32,9 @@ public class UserController {
 
 		String savedFileName = userService.changeProfileImage(request, image, requestUserNo);
 
-		return new CommonResponseDTO(HttpStatus.OK, "이미지 저장 성공 :" + savedFileName);
+		Map<String, Object> body = new HashMap<>();
+		body.put("savedFileName", savedFileName);
+		return new CommonResponseDTO(body, HttpStatus.OK, "이미지 저장 성공 :", null, true);
 	}
 
 }
